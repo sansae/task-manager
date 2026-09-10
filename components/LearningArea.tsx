e814d4f020c9b3fc2660f05e91702ca999a2eb26
Kent Saeteurn <kentsaeteurn@gmail.com>
2026-09-09 22:17:34 -0700

    added basic bar chart using chart.js and canvas

--- components/LearningArea.tsx
+++ components/LearningArea.tsx
@@ -1,3 +1,42 @@
+'use client';
+
+import { useEffect, useRef } from 'react';
+import Chart from 'chart.js/auto';
+
+function AcquisitionChart() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const data = [
+    { year: 2010, count: 10 },
+    { year: 2011, count: 20 },
+    { year: 2012, count: 15 },
+    { year: 2013, count: 25 },
+    { year: 2014, count: 22 },
+    { year: 2015, count: 30 },
+    { year: 2016, count: 28 },
+  ];
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+
+    const chart = new Chart(canvasRef.current, {
+      type: "bar",
+      data: {
+        labels: data.map((row) => row.year),
+        datasets: [
+          {
+            label: 'Acquisitions by year',
+            data: data.map((row) => row.count)
+          }
+        ]
+      }
+    })
+
+    return () => chart.destroy();
+  }, []);
+
+  return <canvas ref={canvasRef} />
+}
+
 export default function LearningArea() {
   return (
     <div className="lg:w-1/2 flex min-h-screen flex-col md:flex-row my-0 mx-auto px-4 py-6 font-sans text-zinc-950 transition-colors dark:bg-zinc-950 dark:text-zinc-50 sm:px-6">
@@ -11,6 +50,9 @@ export default function LearningArea() {
 
         <div className="chart-js">
           <h3>Chart.js example</h3>
+          <div className="w-[500px]">
+            <AcquisitionChart />
+          </div>
         </div>
       </div>
     </div>
